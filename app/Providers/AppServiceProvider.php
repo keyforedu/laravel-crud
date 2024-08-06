3855826d36abdeee9d1baa3a435e0c51ca16ec5a
keyforedu <keyforedu@gmail.com>
2024-08-06 11:16:13 +0700

    update ASP

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,7 +21,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+ 
+        if($this->app->environment('production')){
+            URL::forceScheme('https');
+        };
         Paginator::useBootstrapFive();
     }
 }
